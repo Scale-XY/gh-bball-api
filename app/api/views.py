5211c619647b7cbe445bd660bee0c5341b313f06
JomariRamos <ramosjomariv.vuewin@gmail.com>
2024-06-12 17:03:17 +0800

    bugfix

--- app/api/views.py
+++ app/api/views.py
@@ -11,7 +11,7 @@ from rest_framework.views import APIView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 import csv
 
