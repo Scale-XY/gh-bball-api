60273a7abdce34281b29816762eabfc339833e39
JomariRamos <ramosjomariv.vuewin@gmail.com>
2024-07-04 23:49:50 +0800

    updated model properties

--- app/api/models.py
+++ app/api/models.py
@@ -55,95 +55,170 @@ class Player(models.Model):
     def __str__(self):
         return f"{self.name} {self.jersey_number}"
 
+    def _get_statistics(self, playoff=False):
+        if playoff:
+            return self.statistics.filter(game__playoff_game__isnull=False)
+        return self.statistics.filter(game__playoff_game__isnull=True)
+
+    def _aggregate_statistics(self, stat_field, playoff=False):
+        return self._get_statistics(playoff).aggregate(total=Sum(stat_field))['total'] or 0
+
     @property
     def total_two_point_fg(self):
-        return self.statistics.aggregate(total_two_point_fg=Sum('two_point_fg'))['total_two_point_fg'] or 0
+        return self._aggregate_statistics('two_point_fg')
 
     @property
     def total_three_point_fg(self):
-        return self.statistics.aggregate(total_three_point_fg=Sum('three_point_fg'))['total_three_point_fg'] or 0
+        return self._aggregate_statistics('three_point_fg')
 
     @property
     def total_free_throw_fg(self):
-        return self.statistics.aggregate(total_free_throw_fg=Sum('free_throw_fg'))['total_free_throw_fg'] or 0
+        return self._aggregate_statistics('free_throw_fg')
 
     @property
     def total_points(self):
-        stats = self.statistics.aggregate(
-            total_two_point_fg=Sum('two_point_fg'),
-            total_three_point_fg=Sum('three_point_fg'),
-            total_free_throw_fg=Sum('free_throw_fg')
+        return (
+            self.total_two_point_fg * 2 +
+            self.total_three_point_fg * 3 +
+            self.total_free_throw_fg
         )
-        total_two_point_fg = stats['total_two_point_fg'] or 0
-        total_three_point_fg = stats['total_three_point_fg'] or 0
-        total_free_throw_fg = stats['total_free_throw_fg'] or 0
-        return (total_two_point_fg * 2) + (total_three_point_fg * 3) + total_free_throw_fg
 
     @property
     def total_rebounds(self):
-        stats = self.statistics.aggregate(
-            total_offensive_rebounds=Sum('offensive_rebounds'),
-            total_defensive_rebounds=Sum('defensive_rebounds')
+        return (
+            self._aggregate_statistics('offensive_rebounds') +
+            self._aggregate_statistics('defensive_rebounds')
         )
-        total_offensive_rebounds = stats['total_offensive_rebounds'] or 0
-        total_defensive_rebounds = stats['total_defensive_rebounds'] or 0
-        return total_offensive_rebounds + total_defensive_rebounds
 
     @property
     def total_assists(self):
-        return self.statistics.aggregate(total_assists=Sum('assists'))['total_assists'] or 0
+        return self._aggregate_statistics('assists')
 
     @property
     def total_steals(self):
-        return self.statistics.aggregate(total_steals=Sum('steals'))['total_steals'] or 0
+        return self._aggregate_statistics('steals')
 
     @property
     def total_blocks(self):
-        return self.statistics.aggregate(total_blocks=Sum('blocks'))['total_blocks'] or 0
+        return self._aggregate_statistics('blocks')
 
     @property
     def total_fouls(self):
-        return self.statistics.aggregate(total_fouls=Sum('fouls'))['total_fouls'] or 0
+        return self._aggregate_statistics('fouls')
 
     @property
     def average_points_per_game(self):
-        total_games = self.statistics.count()
+        total_games = self._get_statistics().count()
         if total_games > 0:
             return round(self.total_points / total_games, 1)
-        else:
-            return 0
+        return 0
 
     @property
     def average_rebounds_per_game(self):
-        total_games = self.statistics.count()
+        total_games = self._get_statistics().count()
         if total_games > 0:
             return round(self.total_rebounds / total_games, 1)
-        else:
-            return 0
+        return 0
 
     @property
     def average_assists_per_game(self):
-        total_games = self.statistics.count()
+        total_games = self._get_statistics().count()
         if total_games > 0:
             return round(self.total_assists / total_games, 1)
-        else:
-            return 0
+        return 0
 
     @property
     def average_blocks_per_game(self):
-        total_games = self.statistics.count()
+        total_games = self._get_statistics().count()
         if total_games > 0:
             return round(self.total_blocks / total_games, 1)
-        else:
-            return 0
+        return 0
 
     @property
     def average_steals_per_game(self):
-        total_games = self.statistics.count()
+        total_games = self._get_statistics().count()
         if total_games > 0:
             return round(self.total_steals / total_games, 1)
-        else:
-            return 0
+        return 0
+
+    # Playoff statistics
+    @property
+    def total_playoff_two_point_fg(self):
+        return self._aggregate_statistics('two_point_fg', playoff=True)
+
+    @property
+    def total_playoff_three_point_fg(self):
+        return self._aggregate_statistics('three_point_fg', playoff=True)
+
+    @property
+    def total_playoff_free_throw_fg(self):
+        return self._aggregate_statistics('free_throw_fg', playoff=True)
+
+    @property
+    def total_playoff_points(self):
+        return (
+            self.total_playoff_two_point_fg * 2 +
+            self.total_playoff_three_point_fg * 3 +
+            self.total_playoff_free_throw_fg
+        )
+
+    @property
+    def total_playoff_rebounds(self):
+        return (
+            self._aggregate_statistics('offensive_rebounds', playoff=True) +
+            self._aggregate_statistics('defensive_rebounds', playoff=True)
+        )
+
+    @property
+    def total_playoff_assists(self):
+        return self._aggregate_statistics('assists', playoff=True)
+
+    @property
+    def total_playoff_steals(self):
+        return self._aggregate_statistics('steals', playoff=True)
+
+    @property
+    def total_playoff_blocks(self):
+        return self._aggregate_statistics('blocks', playoff=True)
+
+    @property
+    def total_playoff_fouls(self):
+        return self._aggregate_statistics('fouls', playoff=True)
+
+    @property
+    def average_playoff_points_per_game(self):
+        total_games = self._get_statistics(playoff=True).count()
+        if total_games > 0:
+            return round(self.total_playoff_points / total_games, 1)
+        return 0
+
+    @property
+    def average_playoff_rebounds_per_game(self):
+        total_games = self._get_statistics(playoff=True).count()
+        if total_games > 0:
+            return round(self.total_playoff_rebounds / total_games, 1)
+        return 0
+
+    @property
+    def average_playoff_assists_per_game(self):
+        total_games = self._get_statistics(playoff=True).count()
+        if total_games > 0:
+            return round(self.total_playoff_assists / total_games, 1)
+        return 0
+
+    @property
+    def average_playoff_blocks_per_game(self):
+        total_games = self._get_statistics(playoff=True).count()
+        if total_games > 0:
+            return round(self.total_playoff_blocks / total_games, 1)
+        return 0
+
+    @property
+    def average_playoff_steals_per_game(self):
+        total_games = self._get_statistics(playoff=True).count()
+        if total_games > 0:
+            return round(self.total_playoff_steals / total_games, 1)
+        return 0
 
 class PlayerStatistics(models.Model):
     player = models.ForeignKey(Player, related_name='statistics', on_delete=models.CASCADE)
